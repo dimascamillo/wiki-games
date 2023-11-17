a1c13498936b18ab10e3b7de5b3885fad346f999
Dimas Camillo <dimas.camillo@sonda.com>
2023-11-17 09:23:26 -0300

    Character Card functionality and skills menu have been added

--- src/pages/Category/callOfDragons.tsx
+++ src/pages/Category/callOfDragons.tsx
@@ -1,9 +1,33 @@
 import character from "../../assets/call-of-dragons/PetBirdPeakRoc.png";
 
 export function CallOfDragons() {
+  function handleShowHAbilitys() {
+    const trigger = document.getElementById("menu-habilities");
+
+    trigger && trigger.classList.toggle("hidden");
+  }
+
   return (
-    <figure className="w-96 mt-6">
-      <img src={character} className="w-full object-cover rounded-md" />
-    </figure>
+    <main className="mt-10">
+      <h2 className="w-full">Selecione o seu personagem</h2>
+      <div className="flex">
+        <button onClick={handleShowHAbilitys}>
+          <figure className="w-56 p-8 mt-6 bg-slate-500 rounded-lg relative">
+            <img src={character} className="w-full object-cover" />
+            <span className="absolute bottom-2 right-3 text-cyan-50">Pet Bird Peak Rock</span>
+          </figure>
+        </button>
+
+        <aside id="menu-habilities" className="p-8 hidden">
+          <h2>Selecione as Habilidades</h2>
+          <select className="w-full h-10 mt-5 bg-slate-600">
+            <option value="">Selecione uma habilidade</option>
+            <option value="">1</option>
+            <option value="">2</option>
+            <option value="">3</option>
+          </select>
+        </aside>
+      </div>
+    </main>
   );
 }
--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,3 +1,12 @@
+import { NavLink } from "react-router-dom";
+import { CharactersCard } from "../../components/charactersCard";
+
 export function Category() {
-  return <h1>Category</h1>;
+  return (
+    <div className="mt-10">
+      <NavLink to="/category/callofdragons">
+        <CharactersCard />
+      </NavLink>
+    </div>
+  );
 }
