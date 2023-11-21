cd74b054fce8b82f5c9799cbbf911c74f123e499
Dimas Camillo <dimas.camillo@sonda.com>
2023-11-21 17:10:21 -0300

    Finished release 01

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -4,6 +4,7 @@ import { CharactersCard } from "../../components/charactersCard";
 export function Category() {
   return (
     <div className="mt-10">
+      <h2 className="mb-5">Todas as categorias</h2>
       <NavLink to="/category/callofdragons">
         <CharactersCard />
       </NavLink>
